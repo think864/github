bae8d49a83717123cbc28f16bf6f64d858e738f6
wenhao <wenhao@sias.com>
2022-08-16 15:51:11 +0800

    first commit

---
+++ src/com/itwenhao/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.itwenhao.git;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("hello git!!!!");
+        System.out.println("hello git2!!!");
+        System.out.println("hello git3!!!");
+        System.out.println("hello git4!!!");
+
+
+	// write your code here
+    }
+}
